6c62bf165f1dfc826d25a73ac01b4249698f6f01
Harry <burundukovethereum@mail.ru>
2023-01-27 22:32:21 +0300

    Create app
    Create a simple application "To do list"

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500))
+    done = db.Column(db.Boolean)
+
+
+@app.route('/')
+def index():
+    todo_list = ToDo.query.all()
+    return render_template('index.html', todo_list=todo_list)
+
+
+@app.route('/add', methods=['post'])
+def name():
+    name = request.form.get("name")
+    new_task = ToDo(name=name, done=False)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = ToDo.query.get(todo_id)
+    todo.done = not todo.done
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = ToDo.query.get(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
